9f898c95e5f1a925255ede0044194a6e4b86a60e
Chris Reeves <hello@chris.reeves.io>
2014-10-08 10:34:12 +0100

    Fixes to run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ set -e
 # Default python is 2 and bin 2.7
 PYTHON_VERSION=${PYTHON_VERSION:-"2"}
 PYTHON_BIN=/usr/bin/python2.7
-PIP_BIN=/usr/bin/pip2
-SPHINX_BUILD_BIN=/usr/bin/sphinx-build
+PIP_BIN=/usr/local/bin/pip2
+SPHINX_BUILD_BIN=/usr/local/bin/sphinx-build
+CODE_DIR=${CODE_DIR:-"/code"}
+DOCS_DIR=${DOCS_DIR:-"docs"}
+OUT_DIR=${OUT_DIR:-"/out"}
 
 # Default version control is git
 VERSION_CONTROL=${VERSION_CONTROL:-"git"}
@@ -14,7 +17,8 @@ VERSION_CONTROL=${VERSION_CONTROL:-"git"}
 if [ $PYTHON_VERSION == "3" ]
 then
     PYTHON_BIN=/usr/bin/python3
-    PIP_BIN==/usr/bin/pip3
+    PIP_BIN=/usr/local/bin/pip3
+
 fi
 
 if [ $VERSION_CONTROL == "git" ]
@@ -22,16 +26,11 @@ then
     VERSION_CONTROL_BIN=/usr/bin/git
 fi
 
-# Clone the repository
-mkdir -p /repository
-mkdir -p /docs/html
-$VERSION_CONTROL_BIN clone ${REPOSITORY} /repository
-
 # Install the Python Package
-$PYTHON_BIN /repository/setup.py install
+$PYTHON_BIN $CODE_DIR/setup.py install
 
 # Install Other Package Dependencies
-$PIP_BIN install -r REQS.DEVELOP.txt
+$PIP_BIN install -r $CODE_DIR/REQS.DEVELOP.txt
 
 # Generate the Sphinx Docs
-$SPHINX_BUILD_BIN -b html /repository/docs /docs/html
+$SPHINX_BUILD_BIN -b html $CODE_DIR/$DOCS_DIR $OUT_DIR
